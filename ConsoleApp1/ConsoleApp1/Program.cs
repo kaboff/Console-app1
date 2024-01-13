e9a34bd1f69982b232f899266cc83fba283baf60
Дмитрий <kaboff2@live.com>
2024-01-13 13:13:43 +0100

    part2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool Woman = true;
+int age = 18;
+string name = "Ewa";
+
+if (Woman == true && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name == "Ewa" && age == 31)
+{
+    Console.WriteLine("Ewa, lat 31");
+}
+else if (Woman == false && age < 18) 
+{
+    Console.WriteLine("Niepelnoletni men");
+}
