4aa68b674bedd052ca8f6b9c659040ce6c45bf9e
Дмитрий <kaboff2@live.com>
2024-01-15 18:28:11 +0100

    part4

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,8 +1,8 @@
-﻿bool Woman = true;
+﻿bool woman = true;
 int Age = 18;
 string name = "Ewa";
 
-if (Woman == true && Age < 30)
+if (woman == true && Age < 30)
 {
     Console.WriteLine("Kobieta ponizej 30 lat");
 }
@@ -10,7 +10,7 @@ else if (name == "Ewa" && Age == 31)
 {
     Console.WriteLine("Ewa, lat 31");
 }
-else if (Woman == false && Age < 18) 
+else if (woman == false && Age < 18) 
 {
     Console.WriteLine("Niepelnoletni men");
 }
