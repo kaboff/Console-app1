6135f5896640f2eaf4aac0d13c58d428e414188e
Дмитрий <kaboff2@live.com>
2024-01-23 17:34:30 +0100

    par7

---
+++ ConsoleApp1/ConsoleApp1/Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1;
+
+public class Employee
+{
+    private List<int> point = new List<int>();
+    public Employee(string name, string lastname, int age)
+    {
+        this.Name = name;
+        this.Lastname = lastname;
+        this.Age = age;
+    }
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public int Age { get; set; }
+    public int Result
+    {
+        get
+        {
+                return this.point.Sum();
+        }
+    }
+    public void AddScore(int number)
+    {
+            this.point.Add(number);
+     }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,69 +1,45 @@
-﻿
-var number = 45812589;
-string numberIntString = number.ToString();
-char[] letters = numberIntString.ToArray();
-var number0 = 0;
-var number1 = 0;
-var number2 = 0;
-var number3 = 0;
-var number4 = 0;
-var number5 = 0;
-var number6 = 0;
-var number7 = 0;
-var number8 = 0;
-var number9 = 0;
-foreach (char letter in letters)
+﻿using ConsoleApp1;
+
+var employee1 = new Employee("Mirek", "Gielda", 19);
+var employee2 = new Employee("Sonia", "Gielda", 23);
+var employee3 = new Employee("Marek", "Gielda", 25);
+
+employee1.AddScore(3);
+employee1.AddScore(3);
+employee1.AddScore(3);
+employee1.AddScore(3);
+employee1.AddScore(4);
+
+employee2.AddScore(5);
+employee2.AddScore(5);
+employee2.AddScore(5);
+employee2.AddScore(5);
+employee2.AddScore(6);
+
+employee3.AddScore(7);
+employee3.AddScore(7);
+employee3.AddScore(7);
+employee3.AddScore(7);
+employee3.AddScore(8);
+
+List<Employee> employees = new List<Employee>()
 {
-    if (letter == '0')
+    employee1,employee2, employee3
+};
+
+
+int maxResult = -1;
+Employee employeeWitchMaxResult = null;
+
+
+foreach (var employee in employees)
+{
+    if (employee.Result > maxResult)
     {
-        number0++;
+        employeeWitchMaxResult = employee;
     }
-    else if (letter == '1')
-    {
-        number1++;
-    }
-    else if (letter == '2')
-    {
-        number2++;
-    }
-    else if (letter == '3')
-    {
-        number3++;
-    }
-    else if (letter == '4')
-    {
-        number4++;
-    }
-    else if (letter == '5')
-    {
-        number5++;
-    }
-    else if (letter == '6')
-    {
-        number6++;
-    }
-    else if (letter == '7')
-    {
-        number7++;
-    }
-    else if (letter == '8')
-    {
-        number8++;
-    }
-    else if (letter == '9')
-    {
-        number9++;
-    }
+    
 }
-Console.WriteLine("0=>" + number0);
-Console.WriteLine("1=>" + number1);
-Console.WriteLine("2=>" + number2);
-Console.WriteLine("3=>" + number3);
-Console.WriteLine("4=>" + number4);
-Console.WriteLine("5=>" + number5);
-Console.WriteLine("6=>" + number6);
-Console.WriteLine("7=>" + number7);
-Console.WriteLine("8=>" + number8);
-Console.WriteLine("9=>" + number9);
 
-
+Console.WriteLine(employeeWitchMaxResult.Name + employeeWitchMaxResult.Lastname +
+  " " + employeeWitchMaxResult.Age + maxResult );
