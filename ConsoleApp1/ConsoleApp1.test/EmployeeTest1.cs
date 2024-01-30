7106eb1b6b681dc773f46bbe8c1386fe35cf9c71
Дмитрий <kaboff2@live.com>
2024-01-30 17:06:35 +0100

    part10

--- ConsoleApp1/ConsoleApp1.test/EmployeeTest1.cs
+++ ConsoleApp1/ConsoleApp1.test/EmployeeTest1.cs
@@ -18,39 +18,6 @@ namespace ConsoleApp1.test
             Assert.AreEqual(14, result);
         }
 
-        [Test]
-        public void AAddingnegativenumbers()
-        {
-            // arrange
-            var employee2 = new Employee("Sonia", "Gielda", 23);
-            employee2.AddScore(-5);
-            employee2.AddScore(-6);
-            employee2.AddScore(-3);
-
-            // act
-            var result = employee2.Result;
-
-            //assert
-            Assert.AreEqual(-14, result);
-
-
-        }
-
-        [Test]
-        public void Additionofmixednumbers()
-        {
-            // arrange
-            var employee3 = new Employee("Marek", "Gielda", 25);
-            employee3.AddScore(5);
-            employee3.AddScore(-6);
-            employee3.AddScore(-3);
-
-            // act
-            var result = employee3.Result;
-
-            //assert
-            Assert.AreEqual(-4, result);
-
-        }
+        
     }
 }
---
+++ ConsoleApp1/ConsoleApp1.test/TypeTest.cs
@@ -0,0 +1,50 @@
+﻿
+namespace ConsoleApp1.test
+{
+    public class TypeTest
+    {
+        [Test]
+        public void Comparisonofnumbers()
+        {
+            // arrange
+            int number1 = 25;
+            int number2 = 25;
+            // act
+
+
+            //assert
+            Assert.AreEqual(number1, number2);
+        }
+        [Test]
+        public void Othercustomcheck()
+        {
+            // arrange
+            var user1 = GetUser("Dima","lock",23);
+            var user2 = GetUser("Dima","lock",21);
+            // act
+
+
+            //assert
+            Assert.AreNotEqual(user1, user2);
+        }
+        [Test]
+        public void checkingloginvalue()
+        {
+            // arrange
+            var user1 = GetUser("Dima","lock",23);
+            var user2 = GetUser("Dima","lock",21);
+            // act
+
+
+            //assert
+            Assert.AreNotEqual(user1, user2);
+        }
+
+        private Employee GetUser(string name,string password,int age)
+        {
+            return new Employee ( name,password,age);
+        }
+
+    }
+
+}
